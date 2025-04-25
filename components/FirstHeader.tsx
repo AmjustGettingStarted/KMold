fa7ee0f787d88deab58220d0622c1f9c5d68d691
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-25 15:38:07 +0530

    added location selector

--- components/FirstHeader.tsx
+++ components/FirstHeader.tsx
@@ -1,33 +1,35 @@
 import Image from "next/image";
 import React from "react";
-import { IoIosArrowDown } from "react-icons/io";
 import MarutiSuzuki from "../assets/maruti_suzuki.png";
 import Call from "../assets/call.png";
+import { SelectMenu } from "./sub/SelectMenu";
 const FirstHeader = () => {
   return (
     <header>
       <div className="sm:static z-20 fixed top-0 left-0 bg-white flex justify-between w-full items-center py-2 px-5 ">
         {/* Header Left Section */}
-        <div className="flex gap-5 items-center justify-start">
+        <div className="flex gap-2 items-center justify-start">
           <div>
             <Image
               src="https://www.kalyanimotors.com/static/media/kalyani_dark.5bca4adb5ec73478b2ce.png"
               alt="Kalyani Motors Logo"
               width={120}
               height={45}
-              className="w-[100px] h-[40px] sm:w-[120px] sm:h-[45px]"
+              className="w-[100px] h-[40px] sm:w-[120px] sm:h-[45px] cursor-pointer"
             />
           </div>
-          <div className="hidden sm:flex flex-row gap-1 items-center">
-            <span className="-tracking-tighter text-xs">Bengaluru</span>
+          <div className="hidden sm:flex  items-center cursor-pointer">
+            {/* <div className="hidden sm:flex flex-row gap-1 items-center cursor-pointer"> */}
+            <SelectMenu />
+            {/* <span className="-tracking-tighter text-xs">Bengaluru</span>
             <span className="opacity-50">
               <IoIosArrowDown size={13} />
-            </span>
+            </span> */}
           </div>
         </div>
         {/* Header Right Section */}
         <div className="flex flex-row items-center gap-8 ">
-          <div className="flex items-center flex-row gap-1 border border-[#1e5d8f] p-1 rounded-md">
+          <div className="flex items-center flex-row gap-1 border border-[#1e5d8f] p-1 cursor-pointer rounded-md">
             <span>
               <Image src={Call} alt="call logo" className="sm:size-4 size-2 " />
             </span>
