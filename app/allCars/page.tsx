1027774b2ee1125df8bf35c8baff6482a308b019
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-26 18:12:00 +0530

    Removed {title} since it's not receiving any dynamic values

--- app/allCars/page.tsx
+++ app/allCars/page.tsx
@@ -1,3 +1,12 @@
+<<<<<<< HEAD
+// "use client";
+// import { useParams } from "next/navigation";
+import React from "react";
+
+const AllCars = () => {
+  // const { title } = useParams() as { title: string };
+  return <div>AllCars </div>;
+=======
 "use client";
 import { useParams } from "next/navigation";
 import React from "react";
@@ -5,6 +14,7 @@ import React from "react";
 const AllCars = () => {
   const { title } = useParams() as { title: string };
   return <div>AllCars: {title}</div>;
+>>>>>>> c6565afdea912e3f29472f9eaa885e0329b5ecda
 };
 
 export default AllCars;
