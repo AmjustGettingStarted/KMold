07ed0820e3806c0bedefcd34859a13ce26e983cf
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-26 14:25:44 +0530

    paddingtop to deal with sticky header

--- app/details/[title]/page.tsx
+++ app/details/[title]/page.tsx
@@ -4,7 +4,7 @@ import { useParams } from "next/navigation";
 const CarDetails = () => {
   const { title } = useParams() as { title: string };
   return (
-    <div className="w-full h-[50vh] text-4xl ">
+    <div className="w-full h-[50vh] text-4xl relative pt-[50px] sm:pt-0 ">
       <h1>CarDetails :{decodeURIComponent(title).replace("%", " ")}</h1>
     </div>
   );
