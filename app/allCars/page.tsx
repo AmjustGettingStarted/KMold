d5c6b95474a42500765decdef15591f55a6130da
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-28 09:54:58 +0530

    removed the dynamic receiving of data {title}

--- app/allCars/page.tsx
+++ app/allCars/page.tsx
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-// "use client";
-// import { useParams } from "next/navigation";
 import React from "react";
 
 const AllCars = () => {
-  // const { title } = useParams() as { title: string };
-  return <div>AllCars </div>;
-=======
-"use client";
-import { useParams } from "next/navigation";
-import React from "react";
-
-const AllCars = () => {
-  const { title } = useParams() as { title: string };
-  return <div>AllCars: {title}</div>;
->>>>>>> c6565afdea912e3f29472f9eaa885e0329b5ecda
+  return <div>AllCars</div>;
 };
 
 export default AllCars;
