808f4e83bad9278ca28e0f8a9d3da05715fafd86
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-28 17:18:52 +0530

    carDetails Carousel type

--- components/sub/HorizontalCard.tsx
+++ components/sub/HorizontalCard.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { CarDetailsSelect } from "./CarDetailsSelect";
+import { carImage } from "@/data/carDetailsImages";
+
 export interface Cars {
   id: number;
   url: string;
@@ -12,17 +15,44 @@ interface CardProps {
   carDetails: Cars;
 }
 const HorizontalCard: React.FC<CardProps> = ({ carDetails }) => {
+  const [iniImage, setIniImage] = useState(carDetails.url);
+  const handleClick = (id: number) => {
+    setIniImage(id === 0 ? carDetails.url : carImage[id].image);
+  };
+
   return (
     <div className="w-full border border-gray-300">
       <div className="grid grid-cols-2">
-        <div>
-          <Image
-            src={carDetails.url}
-            alt={carDetails.title}
-            width={500}
-            height={500}
-          />
+        {/* left side */}
+        <div className="w-full flex flex-col space-y-2">
+          {/* Top Image */}
+          <div className="flex items-center justify-center object-contain bg-[#f5deb3]">
+            <Image
+              src={iniImage}
+              alt={carDetails.title}
+              width={500}
+              height={500}
+            />
+          </div>
+          {/* Bottom Display Images */}
+          <div className="w-full grid grid-cols-9 gap-2">
+            {carImage.map((item, index) => (
+              <div
+                key={index}
+                className="w-[45px] h-[30px] onclick cursor-pointer"
+              >
+                <Image
+                  src={index === 0 ? carDetails.url : item.image}
+                  alt="carimages"
+                  width={45}
+                  height={30}
+                  onClick={() => handleClick(item.id)}
+                />
+              </div>
+            ))}
+          </div>
         </div>
+        {/* right side */}
         <div className="flex flex-col">
           <div className="flex w-full ">
             <div className="w-full border border-gray-300 py-4 px-4 ">
@@ -33,7 +63,7 @@ const HorizontalCard: React.FC<CardProps> = ({ carDetails }) => {
             </div>
             <div className="w-full border border-gray-300 py-4 px-4">
               <p className="tracking-tight text-sm text-[#474747]">Color</p>
-              <h1 className="text-sm font-semibold">Gallant Red</h1>
+              <h1 className="text-sm font-semibold py-2">Gallant Red</h1>
             </div>
           </div>
           <div className="py-4 px-2 space-y-4">
@@ -44,7 +74,7 @@ const HorizontalCard: React.FC<CardProps> = ({ carDetails }) => {
               <div className="flex flex-col leading-tight ">
                 <p>Calculate Your EMI</p>
                 <p className="text-xs text-[#477Ed6] font-semibold">
-                  EMI calculator{" "}
+                  EMI calculator
                 </p>
               </div>
               <button className="border text-sm px-6 py-2 text-[#00afa0] border-[#00afa0]">
---
+++ data/carDetailsImages.ts
@@ -0,0 +1,91 @@
+export interface CarImages {
+  id: number;
+  image: string;
+}
+export const carImage: CarImages[] = [
+  {
+    id: 0,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/xAt6rWoJdF_0_1731569298.jpg",
+  },
+  {
+    id: 1,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_1_1731579395.jpg",
+  },
+  {
+    id: 2,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_16_1731579395.jpg",
+  },
+  {
+    id: 3,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_2_1731579395.jpg",
+  },
+  {
+    id: 4,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_3_1731579395.jpg",
+  },
+  {
+    id: 5,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_4_1731579395.jpg",
+  },
+  {
+    id: 6,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_5_1731579395.jpg",
+  },
+  {
+    id: 7,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_6_1731579395.jpg",
+  },
+  {
+    id: 8,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_7_1731579395.jpg",
+  },
+  {
+    id: 9,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_8_1731579395.jpg",
+  },
+  {
+    id: 10,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_9_1731579395.jpg",
+  },
+  {
+    id: 11,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_10_1731579395.jpg",
+  },
+  {
+    id: 12,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_11_1731579395.jpg",
+  },
+  {
+    id: 13,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_12_1731579395.jpg",
+  },
+  {
+    id: 14,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_13_1731579395.jpg",
+  },
+  {
+    id: 15,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_14_1731579395.jpg",
+  },
+  {
+    id: 16,
+    image:
+      "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/YHcSGSyQ0S_15_1731579395.jpg",
+  },
+];
