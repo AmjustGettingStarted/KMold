a9747eecf7fa610d077824a5ba13a4b8ac7cd2a9
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-29 16:29:49 +0530

    Raw Data containing details of car variants

---
+++ data/carVariants.ts
@@ -0,0 +1,54 @@
+type Prop = {
+  title: string;
+  price: string;
+  url: string;
+};
+
+export const variants: Prop[] = [
+  {
+    title: "New Epic Swift VXI AMT",
+    price: "₹7,79,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/XrYDZZQlHa_0_1731481119.jpg",
+  },
+  {
+    title: "New Epic Swift VXI (O)",
+    price: "₹7,56,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/TiFJ1gxxet_0_1731481802.jpg",
+  },
+  {
+    title: "New Epic Swift VXI (O) AMT",
+    price: "₹8,06,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/5TFSJVmmrN_0_1731482416.jpg",
+  },
+  {
+    title: "New Epic Swift ZXI",
+    price: "₹8,29,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/kNkIKWTnsm_0_1731482970.jpg",
+  },
+  {
+    title: "New Epic Swift ZXI AMT",
+    price: "₹8,79,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/y0F4bEPYlK_0_1731493266.jpg",
+  },
+  {
+    title: "New Epic Swift ZXI +",
+    price: "₹8,99,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/VsKCcZArw4_0_1731493383.jpg",
+  },
+  {
+    title: "New Epic Swift ZXI + AMT",
+    price: "₹9,49,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/wvosEUNK2u_0_1731493436.jpg",
+  },
+  {
+    title: "New Epic Swift LXI",
+    price: "₹6,49,000 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/Vcd0UlnnGH_0_1731492965.jpg",
+  },
+  {
+    title: "New Epic Swift VXI",
+    price: "₹7,29,500 /-",
+    url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/UIZgf82ZnK_0_1731493872.jpg",
+  },
+  // { title: "", price: "", url: "" },
+];
