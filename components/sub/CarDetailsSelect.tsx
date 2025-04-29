0518e4ce3b66516e4d271b38bb8a562c9b6ff96d
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-29 18:14:54 +0530

    We can change the image of the card by select car variants in the select menu/dropdown menu

--- components/sub/CarDetailsSelect.tsx
+++ components/sub/CarDetailsSelect.tsx
@@ -1,32 +1,37 @@
 import * as React from "react";
+import { useRouter } from "next/navigation";
 import { variants } from "@/data/carVariants";
 import {
   Select,
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
 
 export function CarDetailsSelect({ title }: { title: string }) {
+  const router = useRouter();
+
+  const handleVariantChange = (value: string) => {
+    router.push(`/details/${value.replaceAll(" ", "_")}`);
+  };
+
   return (
-    <Select>
+    <Select onValueChange={handleVariantChange}>
       <SelectTrigger className="px-2 text-[13px] tracking-tighter border border-gray-300 rounded-xs size-[200px]">
         <SelectValue
           placeholder={title}
-          className="-ml-2 placeholder:text-black "
+          className="-ml-2 placeholder:text-black"
         />
       </SelectTrigger>
-      <SelectContent className="border-none ">
+      <SelectContent className="border-none">
         <SelectGroup>
-          <SelectLabel></SelectLabel>
           {variants.map((item, i) => (
             <SelectItem
+              key={i}
               className="text-black hover:bg-gray-500 hover:text-white"
               value={item.title}
-              key={i}
             >
               {i === 0 ? title : item.title}
             </SelectItem>
