689884b5a5cb7b7d2bac971a44e8a601994bf207
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-30 09:47:55 +0530

    Added the key -> id to the variants[]

--- data/carVariants.ts
+++ data/carVariants.ts
@@ -1,4 +1,5 @@
 type Prop = {
+  id: number;
   title: string;
   price: string;
   url: string;
@@ -6,49 +7,57 @@ type Prop = {
 
 export const variants: Prop[] = [
   {
+    id: 1,
     title: "New Epic Swift VXI AMT",
     price: "₹7,79,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/XrYDZZQlHa_0_1731481119.jpg",
   },
   {
+    id: 2,
     title: "New Epic Swift VXI (O)",
     price: "₹7,56,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/TiFJ1gxxet_0_1731481802.jpg",
   },
   {
+    id: 3,
     title: "New Epic Swift VXI (O) AMT",
     price: "₹8,06,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/5TFSJVmmrN_0_1731482416.jpg",
   },
   {
+    id: 4,
     title: "New Epic Swift ZXI",
     price: "₹8,29,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/kNkIKWTnsm_0_1731482970.jpg",
   },
   {
+    id: 5,
     title: "New Epic Swift ZXI AMT",
     price: "₹8,79,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/y0F4bEPYlK_0_1731493266.jpg",
   },
   {
+    id: 6,
     title: "New Epic Swift ZXI +",
     price: "₹8,99,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/VsKCcZArw4_0_1731493383.jpg",
   },
   {
+    id: 7,
     title: "New Epic Swift ZXI + AMT",
     price: "₹9,49,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/wvosEUNK2u_0_1731493436.jpg",
   },
   {
+    id: 8,
     title: "New Epic Swift LXI",
     price: "₹6,49,000 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/Vcd0UlnnGH_0_1731492965.jpg",
   },
   {
+    id: 9,
     title: "New Epic Swift VXI",
     price: "₹7,29,500 /-",
     url: "https://kalyanimotorsadmin.kalyanicrm.com/storage/images/UIZgf82ZnK_0_1731493872.jpg",
   },
-  // { title: "", price: "", url: "" },
 ];
