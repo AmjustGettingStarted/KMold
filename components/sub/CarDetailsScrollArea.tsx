ad84e7e8dddce07f4b8406691b880788625e25d1
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-30 09:53:47 +0530

    Minor changes

--- components/sub/CarDetailsScrollArea.tsx
+++ components/sub/CarDetailsScrollArea.tsx
@@ -1,19 +1,13 @@
 import * as React from "react";
-
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import { variants } from "@/data/carVariants";
 import { CarDetailsCard } from "./CarDetailsCard";
-export interface Artwork {
-  artist: string;
-  art: string;
-}
 
 export function CarDetailsScrollArea() {
   return (
     <ScrollArea className="w-full whitespace-nowrap rounded-sm scrollbar-hidden ">
       <div className="flex w-max space-x-4 p-4 hover:shadow-xl scrollbar-hidden">
-        
-        <CarDetailsCard data={variants}/>
+        <CarDetailsCard data={variants} />
       </div>
       <ScrollBar orientation="horizontal" />
     </ScrollArea>
