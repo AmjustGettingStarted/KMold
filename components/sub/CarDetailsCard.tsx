f63f4c40b5d94b1ea2186d9cedfe187348871eab
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-04-30 09:44:48 +0530

    use <Link> so when click on the card it gets redirected to the same page with info of the clicked card getting reflected

--- components/sub/CarDetailsCard.tsx
+++ components/sub/CarDetailsCard.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import Image from "next/image";
+import Link from "next/link";
 
 type Prop = {
   title: string;
@@ -17,24 +18,26 @@ export function CarDetailsCard({ data }: { data: Prop[] }) {
           className="w-[210px] border-none rounded-sm shadow-none hover:shadow-xl shadow-black"
           key={i}
         >
-          <CardContent>
-            <Image
-              src={item.url}
-              alt={item.title}
-              width={200}
-              height={135}
-              className="object-cover"
-            />
-          </CardContent>
-          <CardFooter className="flex flex-col items-start p-1">
-            <h1 className="font-bold" title={item.title}>
-              {item.title.length < 20
-                ? item.title
-                : item.title.slice(0, 20) + "..."}
-            </h1>
-            <h2>{item.price}</h2>
-            <p className="text-xs text-[#828181]">Ex-showroom Price</p>
-          </CardFooter>
+          <Link href={`/details/${item.title.replaceAll(" ", "_")}`}>
+            <CardContent>
+              <Image
+                src={item.url}
+                alt={item.title}
+                width={200}
+                height={135}
+                className="object-cover"
+              />
+            </CardContent>
+            <CardFooter className="flex flex-col items-start p-1">
+              <h1 className="font-bold" title={item.title}>
+                {item.title.length < 20
+                  ? item.title
+                  : item.title.slice(0, 20) + "..."}
+              </h1>
+              <h2>{item.price}</h2>
+              <p className="text-xs text-[#828181]">Ex-showroom Price</p>
+            </CardFooter>
+          </Link>
         </Card>
       ))}
     </>
