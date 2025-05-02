dec2f19352d3f224cb6f016bc93094546f166de6
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-02 09:56:41 +0530

    Fix1: ADD margini top 60px so that the header won't overlap it in mobile view Fix2: <Link> tag to redirect the page to the carDetails page on click

--- app/allCars/page.tsx
+++ app/allCars/page.tsx
@@ -6,47 +6,50 @@ import { CarData } from "@/data/chooseCars";
 import { variants } from "@/data/carVariants";
 import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
 
 const AllCars = () => {
   const [cdata, setCdata] = useState(allCars);
   return (
     <>
-      <div className="w-full p-6 ">
+      <div className="w-full sm:p-6 p-3 mt-[60px] sm:mt-0 ">
         <h1 className="text-lg sm:text-3xl font-bold">
           Maruti Suzuki - All Cars
         </h1>
-        <div className="relative pt-2 sm:pt-4">
+        {/* TABS section */}
+        <div className="relative pt-2 sm:pt-4 ">
           <Tabs defaultValue="all-cars" className="w-full">
             <TabsList className="bg-white  ">
               <TabsTrigger
-                className="data-[state=active]:text-[#5bbe95] data-[state=active]:underline text-[16px] underline-offset-[16px] data-[state=active]:font-bold data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
+                className="text-[10.5px]  data-[state=active]:text-[#5bbe95] data-[state=active]:underline sm:text-[16px] underline-offset-[16px] data-[state=active]:font-bold data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
                 onClick={() => setCdata(allCars)}
                 value="all-cars"
               >
                 All Cars
               </TabsTrigger>
               <TabsTrigger
-                className="data-[state=active]:text-[#5bbe95]  data-[state=active]:underline text-[16px] underline-offset-[16px] data-[state=active]:font-bold data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
+                className="text-[10.5px]  data-[state=active]:text-[#5bbe95]  data-[state=active]:underline sm:text-[16px] underline-offset-[16px] data-[state=active]:font-bold data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
                 onClick={() => setCdata(data)}
                 value="new-cars"
               >
                 New Cars
               </TabsTrigger>
               <TabsTrigger
-                className="data-[state=active]:text-[#5bbe95] data-[state=active]:underline text-[16px] underline-offset-[16px] data-[state=active]:font-bold data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
+                className="text-[10.5px]  data-[state=active]:text-[#5bbe95] data-[state=active]:underline sm:text-[16px] underline-offset-[16px] data-[state=active]:font-bold data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
                 onClick={() => setCdata(CarData)}
                 value="choose-cars"
               >
                 Choose Cars
               </TabsTrigger>
               <TabsTrigger
-                className="data-[state=active]:text-[#5bbe95] data-[state=active]:underline  text-[16px] underline-offset-[16px] data-[state=active]:font-bold  data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
+                className="text-[10.5px]  data-[state=active]:text-[#5bbe95] data-[state=active]:underline  sm:text-[16px] underline-offset-[16px] data-[state=active]:font-bold  data-[state=active]:!shadow-none data-[state=active]:border-b-4 text-[#aaa] "
                 onClick={() => setCdata(variants)}
                 value="car-variants"
               >
                 Car Variants
               </TabsTrigger>
             </TabsList>
+            {/* Individual Tabs Page/Content */}
             <TabsContent
               value={
                 cdata === allCars
@@ -61,26 +64,31 @@ const AllCars = () => {
               <div className="flex flex-col w-full">
                 <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-4 w-full sm:w-[85%] py-2 mx-auto sm:mx-0">
                   {cdata.map((item, i) => (
-                    <div
-                      className="max-w-sm border border-gray-300 rounded-sm cursor-not-allowed"
+                    <Link
                       key={i}
+                      href={`/details/${item.title.replaceAll(" ", "_")}`}
                     >
-                      <div>
-                        <Image
-                          width={900}
-                          height={600}
-                          src={item.url}
-                          alt={item.title}
-                          className="w-full object-cover rounded-t-sm"
-                        />
-                      </div>
+                      <div
+                        className="max-w-sm border border-gray-300 rounded-sm cursor-pointer"
+                        key={i}
+                      >
+                        <div>
+                          <Image
+                            width={900}
+                            height={600}
+                            src={item.url}
+                            alt={item.title}
+                            className="w-full object-cover rounded-t-sm"
+                          />
+                        </div>
 
-                      <h1 className="p-2 text-[10.5px] sm:text-base bg-[#f9f9f9] text-[#8d7b76]">
-                        {item.title.length < 30
-                          ? item.title
-                          : item.title.slice(0, 30) + "..."}
-                      </h1>
-                    </div>
+                        <h1 className="p-2 text-[10.5px] sm:text-base bg-[#f9f9f9] text-[#8d7b76]">
+                          {item.title.length < 30
+                            ? item.title
+                            : item.title.slice(0, 30) + "..."}
+                        </h1>
+                      </div>
+                    </Link>
                   ))}
                 </div>
               </div>
