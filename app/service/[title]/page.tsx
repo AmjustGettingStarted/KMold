e61ba2193dc14c96274d27d07794c39e11c14e70
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-03 11:11:53 +0530

    WebView completed

--- app/service/[title]/page.tsx
+++ app/service/[title]/page.tsx
@@ -24,7 +24,7 @@ const Service = () => {
         <div className="sticky top-[55px] sm:top-[60px] z-10 bg-white shadow-md w-[98%] mx-auto ">
           <ServicePageScrollHeader data={data} />
         </div>
-        <div className="mt-10 relative w-full px-3 sm:px-6">
+        <div className="relative pt-4 w-full px-3 sm:px-6">
           <h1 className="text-base sm:text-3xl font-bold">{decodedTitle}</h1>
           {/* Individual Cards section */}
           <div className="flex flex-col gap-4 w-full py-4">
@@ -35,7 +35,7 @@ const Service = () => {
               >
                 <div className=" flex-col flex  sm:flex-row  w-full">
                   {/* Image section */}
-                  <div className="w-full sm:w-[20%] flex items-center justify-center">
+                  <div className="w-full sm:w-[20%] flex  justify-center sm:justify-start">
                     <Image
                       src={item.url}
                       alt={item.title}
@@ -45,40 +45,48 @@ const Service = () => {
                     />
                   </div>
                   {/* Description Section */}
-                  <div className="w-full sm:w-[80%] h-full">
-                    <div className="flex flex-col w-full p-4 items-start justify-start">
-                      <div className="flex items-center justify-between w-full">
-                        <h1 key={i} className="w-full  ">
+                  <div className="w-full sm:w-[80%] h-full py-2">
+                    <div className="flex flex-col w-full  items-start justify-start">
+                      <div className="flex items-center justify-between w-full ">
+                        <h1 key={i} className="w-full text-lg font-bold ">
                           {item.title}
                         </h1>
-                        <h2>{item.time}</h2>
+                        <h2 className="whitespace-nowrap text-[12px] tracking-tight bg-[#e5e7e9] text-[#707171] w-[40%] text-center py-1">
+                          {item.time}
+                        </h2>
                       </div>
-                      <div className="w-full ">
+                      <div className="w-full py-2">
                         <ul
-                          className="flex items-center gap-4 list-disc marker:text-gray-300 pl-4 marker:pr-0
-                        marker:text-xm"
+                          className="flex items-center list-disc marker:text-[#707171]/50 marker:pr-0
+                        marker:text-xs justify-start gap-6 pl-4"
                         >
-                          <li>
-                            <p>{item.desc1}</p>
+                          <li className="pl-0 ">
+                            <p className="text-[#707171] text-sm">
+                              {item.desc1}
+                            </p>
                           </li>
-                          <li>
-                            <p>{item.desc2}</p>
+                          <li className="pl-0 ">
+                            <p className="text-[#707171] text-sm ">
+                              {item.desc2}
+                            </p>
                           </li>
                         </ul>
                       </div>
-                      <div className="w-full">
-                        <ul className="w-full grid sm:grid-cols-2 sm:justify-between">
+                      <div className="w-full py-2">
+                        <ul className="w-full grid sm:grid-cols-2 sm:justify-between space-y-3">
                           {item.service.map((serv, j) => (
-                            <li className="flex gap-2 items-center" key={j}>
-                              <div className=" flex items-center justify-center w-3 h-3 rounded-full bg-[#D2EFE0] text-[#26B26B]">
-                                <IoIosCheckmark />
+                            <li className="flex gap-2 items-center " key={j}>
+                              <div className=" flex items-center justify-center w-4 h-4 rounded-full bg-[#D2EFE0] text-[#26B26B]">
+                                <IoIosCheckmark size={20} />
                               </div>
-                              <span className=" text-[#707171]">{serv}</span>
+                              <span className=" text-[#707171] tracking-tight text-sm ">
+                                {serv}
+                              </span>
                             </li>
                           ))}
                         </ul>
                       </div>
-                      <Button className="text-[#e73c33] border border-[#e73c33] bg-white rounded-sm">
+                      <Button className="text-[#e73c33] font-bold  border border-[#e73c33] bg-white rounded-xs my-2">
                         Book Your Service
                       </Button>
                     </div>
