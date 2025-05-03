abf5c7075fa3ea1c9ab025c7433831e2e09ff057
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-03 12:42:35 +0530

    Removed the bottom header/navbar which renders on mobile screen from second header component to first header component

--- components/SecondHeader.tsx
+++ components/SecondHeader.tsx
@@ -1,10 +1,6 @@
 import Image from "next/image";
 import React from "react";
 import { navData } from "../data/secondNavbar";
-import { FaCarRear } from "react-icons/fa6";
-import { PiWhatsappLogoThin } from "react-icons/pi";
-import { IoCallOutline } from "react-icons/io5";
-import { IoLocationOutline } from "react-icons/io5";
 import Link from "next/link";
 const SecondHeader = () => {
   return (
@@ -34,27 +30,7 @@ const SecondHeader = () => {
           </ul>
         </div>
       </div>
-      {/* bottom bar */}
-      <div className="fixed bottom-0 left-0 flex sm:hidden w-full h-[60px] bg-[#2d3393] z-20 ">
-        <div className="flex justify-between w-full px-8 py-2 text-white">
-          <span className="flex flex-col space-y-2 items-center">
-            <FaCarRear size={25} />
-            <h2 className="text-[0.65rem] ">Cars</h2>
-          </span>
-          <span className="flex flex-col space-y-1 items-center">
-            <PiWhatsappLogoThin size={25} />
-            <h2 className="text-[0.65rem] ">Whatsapp</h2>
-          </span>
-          <span className="flex flex-col space-y-1 items-center">
-            <IoCallOutline size={25} />
-            <h2 className="text-[0.65rem] ">Call</h2>
-          </span>
-          <span className="flex flex-col space-y-1 items-center">
-            <IoLocationOutline size={25} />
-            <h2 className="text-[0.65rem] ">Contact Us</h2>
-          </span>
-        </div>
-      </div>
+      
     </>
   );
 };
