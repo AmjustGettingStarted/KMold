30eb5ba82d424b175f3484b4d88274c76c98f5dc
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-05 14:53:02 +0530

    About Kalyani and Why kalyani component's data added

--- app/about-us/[title]/_components/AboutKalyani.tsx
+++ app/about-us/[title]/_components/AboutKalyani.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
 
 const AboutKalyani = () => {
   return (
-    <div>AboutKalyani</div>
-  )
-}
+    <div className="w-full py-8 h-auto ">
+      <div className="flex flex-col ">
+        <div>
+          <h1 className="uppercase text-xl font-bold w-full pb-4">
+            about kalyani
+          </h1>
+          <div className="border-b border-[#4d4d4dbe]/50"></div>
+        </div>
+        <div className="text-justify space-y-8 text-[#4d4d4dbe] py-4 leading-loose">
+          <p>
+            Kalyani Motors, renowned for excellence among Maruti Suzuki dealers
+            for nearly two decades, has been at the forefront of Indias
+            automotive transformation. Founded by the visionary Late Sri S.
+            Anantha Raju at Bengaluru in 2006. Today we proudly carry forward
+            his rich heritage under the dynamic leadership of Mr. Mohan Raju,
+            Managing Director of Kalyani Motors.
+          </p>
+          <p>
+            Spreading excellence across India, Kalyani Motors Secured All India
+            Best Performer 2024 Award for All Channels at Maruti Suzuki Dealers
+            Conference for exceptional Sales and Service provided by us at 78
+            Locations in 3 Cities (Bengaluru, Mysuru, Hyderabad) and 2 States,
+            being No.1 Dealers in Karnataka and fastest growing dealers in
+            Hyderabad featuring Arena & NEXA Showrooms, Service Centres, True
+            Value Outlets, Bodyshops, Maruti Driving Schools, commercial
+            outlets, and Service On Wheels (SOW)
+          </p>
+          <p>
+            Kalyani Motors have earned most prestigious Royal Platinum
+            Dealership award, Omega Dealership award and 6 Alpha Dealership
+            Awards Reflects their steady commitment to ensuring customers
+            delight. In alignment with Maruti Suzukis stringent standards and
+            quality, our mission to provide you with the ultimate vehicle
+            purchase and service experience at all our outlets.
+          </p>
+          <p className="font-bold">
+            Be a part of extraordinary expedition as we continuously set new
+            standards in India’s automotive brilliance.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutKalyani
+export default AboutKalyani;
--- app/about-us/[title]/_components/WhyKalyaniMotors.tsx
+++ app/about-us/[title]/_components/WhyKalyaniMotors.tsx
@@ -1,7 +1,66 @@
 import React from "react";
 
 const WhyKalyaniMotors = () => {
-  return <div>WhyKalyaniMotors</div>;
+  return (
+    <div className="w-full py-8 h-auto ">
+      <div className="flex flex-col ">
+        <div>
+          <h1 className="uppercase text-xl font-bold w-full pb-4">
+            why kalyani motors
+          </h1>
+          <div className="border-b border-[#4d4d4dbe]/50"></div>
+        </div>
+        <div className="text-justify space-y-8 text-[#4d4d4dbe] py-4 leading-loose">
+          <p>
+            Embracing the provisions of dynamic markets, Kalyani Group of
+            companies has forged ahead unremittingly since the year 2006. The
+            group ventured into the automobile industry with the setting up of
+            Kalyani Motors, in Bengaluru initially, and has been hitherto
+            recognised for its excellence and notable achievements.
+          </p>
+          <p>
+            With a fruitful and successful relationship with Maruti Suzuki,
+            Kalyani Motors revolutionised the automobile industry in India,
+            gathering many accolades for its expansion in various cities like
+            Ramanagar, Channapatna, Kanakapura & now Hyderabad.
+          </p>
+          <p>
+            As a "Platinum Dealer" for six consecutive years, Kalyani Motors is
+            the authorised sales & service dealer for Maruti Suzuki range of
+            automobiles in Bangalore, with the estimates for the current year
+            turnover standing at over Rs. 13.5 Billion. Kalyani Motors has many
+            firsts and achievements to its credit. The aggressive growth of the
+            company owes to steadfast processes and methodologies for rendering
+            impeccable quality services and establishing lasting customer
+            relationships.
+          </p>
+          <p>
+            Kalyani Motors has a team of over 3467 employees, with nearly 400
+            sales personnel and 450 technical force, and has sold more than
+            95,000 cars within a short span of 10 years. Carrying on the legacy
+            and aspiring higher, Kalyani Motors has now grown big and has
+            successfully created a strong presence in Bengaluru with the 9
+            State-of-the-art showrooms, 10 Service Centres, 5 True Value
+            outlets, 10 Bodyshops, 2 Maruti Driving Schools and recently entered
+            into the premium segment through Nexa outlets in JPNagar &
+            RRNagar-Mysore Rd; and in Hyderabad, with the 1 state-of-the-art
+            showroom, 2 Service Centres, 2 Bodyshops, 1 True Value outlet and
+            India’s largest 200th Nexa outlet in Nagole, Hyderabad. Also, the
+            future holds 3 more outlets in Bangalore and 5 more at Hyderabad.
+          </p>
+          <p>
+            The collective, dedicated and consorted efforts of ‘Team Kalyani’
+            have been instrumental in Kalyani Motors winning accolades and
+            establishing a strong and satisfied customer base. The state-of-art
+            service centre fulfils our penchant for delivering the best to our
+            customers. Our ever-friendly and ever-helpful sales personnel are
+            known for their alacrity and ensure that every visit to Kalyani
+            Motors is pleasant and memorable.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WhyKalyaniMotors;
