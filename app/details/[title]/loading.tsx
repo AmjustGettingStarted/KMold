dd9146222ee3de85eea3b337623026fab5947830
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-05 09:41:57 +0530

    Added the loader css div

--- app/details/[title]/loading.tsx
+++ app/details/[title]/loading.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const loading = () => {
-  return <div>loading</div>;
+  return (
+    <div className="w-full h-screen flex items-center justify-center text-5xl text-red-600">
+      <div className="w-12 h-12 border-4 border-red-600 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
 };
 
 export default loading;
--- app/service/[title]/loading.tsx
+++ app/service/[title]/loading.tsx
@@ -3,8 +3,7 @@ import React from "react";
 const loading = () => {
   return (
     <div className="h-screen text-5xl text-red-600 flex items-center justify-center">
-      loading
-      <div className="w-12 h-12 border-4 border-red-600 border-t-transparent rounded-full animate-spin"></div>{" "}
+      <div className="w-12 h-12 border-4 border-red-600 border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
 };
