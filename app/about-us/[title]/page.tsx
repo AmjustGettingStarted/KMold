4e47c35b987004d58a2f4b3cd8975762745cd432
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-05 11:43:29 +0530

    Renamed the component folder in about-us to _component

--- app/about-us/[title]/page.tsx
+++ app/about-us/[title]/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import AboutKalyani from "./components/AboutKalyani";
-import WhyKalyaniMotors from "./components/WhyKalyaniMotors";
-import QualityPolicy from "./components/QualityPolicy";
-import PrivacyPolicy from "./components/PrivacyPolicy";
-import Awards from "./components/Awards";
-import DrivingSchool from "./components/DrivingSchool";
-import Careers from "./components/Careers";
-import Gallery from "./components/Gallery";
+import AboutKalyani from "./_components/AboutKalyani";
+import WhyKalyaniMotors from "./_components/WhyKalyaniMotors";
+import QualityPolicy from "./_components/QualityPolicy";
+import PrivacyPolicy from "./_components/PrivacyPolicy";
+import Awards from "./_components/Awards";
+import DrivingSchool from "./_components/DrivingSchool";
+import Careers from "./_components/Careers";
+import Gallery from "./_components/Gallery";
 import { about_details } from "@/data/about-us";
 import Link from "next/link";
 
@@ -21,18 +21,6 @@ const page = async ({ params: { title } }: { params: { title: string } }) => {
       <div className="w-full relative">
         <Tabs defaultValue={title} className="relative">
           <TabsList className="w-[90%] mx-auto sticky top-[60px] z-10 cursor-pointer">
-            {/* <TabsTrigger value="about_kalyani">about kalyani</TabsTrigger>
-            <TabsTrigger value="why_kalyani_motors">
-              why kalyani motors
-            </TabsTrigger>
-            <TabsTrigger value="quality_policy">quality policy</TabsTrigger>
-            <TabsTrigger value="privacy_policy">privacy policy</TabsTrigger>
-            <TabsTrigger value="awards_and_achievement">awards and achievement</TabsTrigger>
-            <TabsTrigger value="maruti_driving_school">
-              maruti driving school
-            </TabsTrigger>
-            <TabsTrigger value="careers">careers</TabsTrigger>
-            <TabsTrigger value="gallery">gallery</TabsTrigger> */}
             {about_details.map((items, i) => (
               <Link href={`/about-us/${items}`} key={i}>
                 <TabsTrigger value={items}>
