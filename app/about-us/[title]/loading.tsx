89654a04c9bf05a8d50918486f4a245dc5d28369
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-05 09:49:52 +0530

    Initial commit of about us page

---
+++ app/about-us/[title]/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center text-5xl text-red-600">
+      <div className="w-12 h-12 border-4 border-red-600 border-t-transparent rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default loading;
